91140ec4334622e7982ffcceeceb19d729c3a226
윤규리 <gyuri@yungyuliui-MacBookPro.local>
2026-03-24 18:29:43 +0900

    feature: 라이트/다크모드 구현.

--- Week02/src/App.tsx
+++ Week02/src/App.tsx
@@ -1,12 +1,21 @@
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
-
+import { useTheme } from "./context/ThemeContext";
 
 function App() {
-  return (
-    <div className="todo-container">
-      <h1 className="todo-container__header">KIWI TODO</h1>
+  const { theme, toggleTheme } = useTheme();
 
+  return (
+    <div
+      className={`todo-container ${
+        theme === "dark" ? "bg-zinc-900 text-white" : "bg-white text-black"
+      }`}
+    >
+      <button onClick={toggleTheme}>
+        {theme === "light" ? "다크모드" : "라이트모드"}
+      </button>
+
+      <h1 className="todo-container__header">KIWI TODO</h1>
       <TodoForm />
       <TodoList />
     </div>

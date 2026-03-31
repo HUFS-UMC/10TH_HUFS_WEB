bfc820a0bd753c540c61aec2e83e130ff285d93d
junehuk <93902605+junehuk@users.noreply.github.com>
2026-03-31 14:23:52 +0900

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  console.log(import.meta.env.VITE_TMDB_KEY)
+  return (
+    <div className="bg-red-500 text-white text-3xl">
+      테스트
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import MoviePage from './pages/MoviePage'
+import './index.css'
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <MoviePage />
+  </React.StrictMode>,
+)

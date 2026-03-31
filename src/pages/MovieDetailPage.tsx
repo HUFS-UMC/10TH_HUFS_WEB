d3164800afa20e32c7ff89968335115cdd5dbb8f
윤선민 <yoonsunmin@yunseonmin-ui-MacBookAir.local>
2026-03-31 14:37:03 +0900

    chore: 불필요한 src 폴더 삭제

--- src/pages/MovieDetailPage.tsx
+++
@@ -1,7 +0,0 @@
-import { useParams } from "react-router-dom";
-
-export default function MovieDetailPage() {
-  const { movieId } = useParams();
-
-  return <div className="p-10 text-white">MovieDetailPage {movieId}</div>;
-}

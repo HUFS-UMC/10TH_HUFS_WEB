fff3a4333a7e2b981b82d750dabe91901cc6fd9c
키테 <113652231+seoyeon2022@users.noreply.github.com>
2026-05-04 01:30:48 +0900

    미션1

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import { createBrowserRouter, RouterProvider, type RouteObject } from 'react-router-dom'
+import './App.css'
+import NotFoundPage from './pages/NotFoundPage';
+import SignupPage from './pages/SignupPage';
+import MyPage from './pages/MyPage';
+import LoginPage from './pages/LoginPage';
+import HomeLayout from './layouts/HomeLayout';
+import HomePage from './pages/HomePage';
+import { AuthProvider } from './context/AuthContext';
+
+// publicRoutes: 인증 없이 접근 가능한 라우트
+const publicRoutes:RouteObject[] = [
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <NotFoundPage />,
+    children: [
+      {index:true, element: <HomePage />},
+      {path:'login', element: <LoginPage />},
+      {path:'signup', element: <SignupPage />},
+    ],
+  },
+];
+
+// protectedRoutes: 인증이 필요한 라우트
+const protectedRoutes:RouteObject[] = [
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <NotFoundPage />,
+    children: [
+      {path: 'my', element: <MyPage />},
+    ],
+  },
+];
+
+const router = createBrowserRouter([...publicRoutes, ...protectedRoutes]);
+
+function App() {
+  return (
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  )
+}
+
+export default App;

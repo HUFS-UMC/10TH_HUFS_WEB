184bc64c90bda96646fa7f2944683f1b369cf282
윤선민 <yoonsunmin@yunseonmin-ui-MacBookAir.local>
2026-05-19 18:11:11 +0900

    Week08-mission2

--- Desktop/Week05/mission-2/src/types/auth.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-import { signupSchema } from "../utils/validate";
-
-export type SignupFormValues = z.infer<typeof signupSchema>;
-
-export interface User {
-  email: string;
-  nickname: string;
-}
--- Desktop/Week07/mission_2/README.md
+++
@@ -1,2 +0,0 @@
-# 10TH_HUFS_WEB
-UMC 10기 한국외대 Web 레포지토리
